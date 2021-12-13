33282dbf585b5898585a0a386af0e54b5b6dae93
CHR-onicles <tpandivine48@gmail.com>
2021-12-13 11:10:38 +0000

    finished footer animation and tweaks

--- src/js/gsap_animations.js
+++ src/js/gsap_animations.js
@@ -10,7 +10,8 @@ export const master = () => {
 
     master_tl.add(footer_animation, ">2")
 }
-export const header_animation = () => {
+
+const header_animation = () => {
     const navbar_logo = document.querySelector('.header__navbar-logo');
     const navbar_toggle_btn = document.querySelector('.header__navbar-toggle-btn');
     const nav_list_items = [document.querySelectorAll('.header__navbar-list-item')];
@@ -26,7 +27,7 @@ export const header_animation = () => {
     }
 }
 
-export const hero_animation = () => {
+const hero_animation = () => {
     const hero_items = [
         document.querySelector('.hero__intro-text'),
         document.querySelector('.hero__author-name'),
@@ -34,14 +35,14 @@ export const hero_animation = () => {
         document.querySelector('.hero__short-desc')
     ]
     let hero_tl = gsap.timeline();
-    const rule = CSSRulePlugin.getRule('.hero__intro-text')
     hero_tl.to(hero_items, { opacity: 1, duration: .5, y: 50, stagger: .3 })
 }
 
-export const footer_animation = () => {
+const footer_animation = () => {
     if (window.matchMedia('(min-width: 62em)').matches) { // 992px
         console.log('entered here')
-        // CSSRulePlugin does not work well with pseudo elements/ Sass (idk yet) after hours of trying and checking online...so I created html elements to replace the pseudos
+        // CSSRulePlugin does not work well with pseudo elements/ Sass (idk yet) 
+        // after hours of trying and checking online...so I created html elements to replace the pseudos
         const left_bar = document.querySelector('.footer__social-list-left-bar');
         const right_bar = document.querySelector('.footer__author-email-right-bar');
         const social_icons = document.querySelectorAll('.footer__social-list-item');
@@ -49,7 +50,7 @@ export const footer_animation = () => {
 
         let footer_tl = gsap.timeline();
         footer_tl.to(left_bar, { scaleY: 1, duration: .5, ease: 'Power2.inOut' })
-        .to(right_bar, {scaleY: 1, duration: .5, ease: 'Power2.inOut'}, '')
+            .to(right_bar, {scaleY: 1, duration: .5, ease: 'Power2.inOut'}, '')
             .to(social_icons, {opacity: 1, y: -20, duration: .3, stagger: -.1})
             .to(author_email, {opacity: 1, duration: .5, y: -20}, '-=.3')
 
